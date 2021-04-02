b0974f03d4a82f05aab7bca9cc823afbabca90b2
ZhangGJ <48301286+letitgone@users.noreply.github.com>
2021-04-03 06:55:19 +0800

    Gavin:Optimize the code

--- IPersistence_test/src/main/java/com/oddness/pojo/User.java
+++ IPersistence_test/src/main/java/com/oddness/pojo/User.java
@@ -1,36 +1,20 @@
 package com.oddness.pojo;
 
+import lombok.Data;
+
+import java.io.Serializable;
+
 /**
  * @Author ZhangGJ
  * @Date 2021/04/02 07:27
  */
-public class User {
+@Data
+public class User implements Serializable {
+
+    private static final long serialVersionUID = -5736687012831339245L;
 
     private Integer id;
+
     private String username;
 
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                '}';
-    }
 }
